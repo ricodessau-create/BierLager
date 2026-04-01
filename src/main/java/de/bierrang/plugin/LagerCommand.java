13fb490c5ca65b8bda34063bcf92dc5544884ecc
ricodessau-create <ricodessau@gmail.com>
2026-04-02 01:21:26 +0200

    Create LagerCommand.java

---
+++ src/main/java/de/bierrang/plugin/LagerCommand.java
@@ -0,0 +1,35 @@
+package de.bierrang.plugin;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+public class LagerCommand implements CommandExecutor {
+
+    private final BierLager plugin;
+
+    public LagerCommand(BierLager plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (!(sender instanceof Player p)) {
+            sender.sendMessage("Nur für Spieler!");
+            return true;
+        }
+
+        if (!p.hasPermission("bierlager.admin")) {
+            p.sendMessage(ChatColor.RED + "Keine Rechte.");
+            return true;
+        }
+
+        ItemStack tool = ItemManager.getLagerTool(plugin);
+        p.getInventory().addItem(tool);
+        p.sendMessage(ChatColor.GREEN + "Du hast das Lager-Werkzeug erhalten!");
+        return true;
+    }
+}

592e403c095f5d8bcbfdf12bc17fe2ed09382295
ricodessau-create <ricodessau@gmail.com>
2026-04-23 10:32:31 +0200

    Update BierLager.java

--- src/main/java/de/bierrang/plugin/BierLager.java
+++ src/main/java/de/bierrang/plugin/BierLager.java
@@ -1,5 +1,6 @@
 package de.bierrang.plugin;
 
+import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class BierLager extends JavaPlugin {
@@ -10,11 +11,17 @@ public class BierLager extends JavaPlugin {
     @Override
     public void onEnable() {
         instance = this;
-        
+
         lagerManager = new LagerManager(this);
         lagerManager.load();
 
-        getCommand("bierlager").setExecutor(new LagerCommand(this));
+        PluginCommand cmd = getCommand("bierlager");
+        if (cmd != null) {
+            cmd.setExecutor(new LagerCommand(this));
+        } else {
+            getLogger().severe("Command 'bierlager' fehlt in der plugin.yml!");
+        }
+
         getServer().getPluginManager().registerEvents(new LagerListener(this), this);
 
         getLogger().info("BierLager v3.2.1 geladen!");

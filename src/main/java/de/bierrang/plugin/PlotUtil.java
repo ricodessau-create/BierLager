ec046e2be8c907bec5eb26c2ba82fb2f1f5ffaf4
ricodessau-create <ricodessau@gmail.com>
2026-05-13 12:04:09 +0200

    Refactor isPlotOwner to use BukkitUtil for location
    Updated the isPlotOwner method to use BukkitUtil for location adaptation.

--- src/main/java/de/bierrang/plugin/PlotUtil.java
+++ src/main/java/de/bierrang/plugin/PlotUtil.java
@@ -1,25 +1,27 @@
 package de.bierrang.plugin;
 
-import com.plotsquared.core.location.Location;
+import com.plotsquared.bukkit.util.BukkitUtil;
 import com.plotsquared.core.plot.Plot;
 import org.bukkit.entity.Player;
 
+/**
+ * Hilfsmethoden für PlotSquared-Berechtigungsprüfungen.
+ */
 public class PlotUtil {
 
     /**
      * Gibt true zurück, wenn der Spieler der Owner des Plots ist,
      * auf dem sich der angegebene Block befindet.
-     * Gibt false zurück wenn kein Plot, kein Owner, oder nur Trusted/Member.
+     *
+     * Gibt false zurück wenn:
+     * - der Block nicht auf einem Plot liegt
+     * - der Plot keinen Owner hat
+     * - der Spieler nur Trusted/Member, aber nicht Owner ist
      */
     public static boolean isPlotOwner(Player player, org.bukkit.Location loc) {
         if (loc.getWorld() == null) return false;
 
-        Location psLoc = Location.at(
-                loc.getWorld().getName(),
-                loc.getBlockX(),
-                loc.getBlockY(),
-                loc.getBlockZ()
-        );
+        com.plotsquared.core.location.Location psLoc = BukkitUtil.adapt(loc);
 
         Plot plot = psLoc.getOwnedPlot();
         if (plot == null) return false;

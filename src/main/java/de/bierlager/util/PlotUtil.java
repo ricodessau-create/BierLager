9c16b00de2658ff456183b973a6e5b33674d8024
ricodessau-create <ricodessau@gmail.com>
2026-05-15 00:02:44 +0200

    Refactor PlotUtil to remove unused PlotAPI initialization

--- src/main/java/de/bierlager/util/PlotUtil.java
+++ src/main/java/de/bierlager/util/PlotUtil.java
@@ -1,7 +1,6 @@
 package de.bierlager.util;
 
-import com.plotsquared.core.PlotAPI;
-import com.plotsquared.core.location.Location;
+import com.plotsquared.bukkit.util.BukkitUtil;
 import com.plotsquared.core.plot.Plot;
 import com.plotsquared.core.plot.PlotId;
 import org.bukkit.entity.Player;
@@ -10,27 +9,20 @@ import java.util.UUID;
 
 public class PlotUtil {
 
-    private static PlotAPI api;
-
-    public static void init() {
-        api = new PlotAPI();
-    }
+    public static void init() {}
 
     public static Plot getPlotAt(org.bukkit.Location loc) {
         if (loc.getWorld() == null) return null;
-        Location psLoc = Location.at(
-                loc.getWorld().getName(),
-                loc.getBlockX(),
-                loc.getBlockY(),
-                loc.getBlockZ()
-        );
+        com.plotsquared.core.location.Location psLoc = BukkitUtil.adapt(loc);
         return psLoc.getPlot();
     }
 
     public static boolean isOwnerOrTrusted(Player player, Plot plot) {
         if (plot == null) return false;
         UUID uuid = player.getUniqueId();
-        return plot.isOwner(uuid) || plot.getTrusted().contains(uuid) || plot.getMembers().contains(uuid);
+        return plot.isOwner(uuid)
+                || plot.getTrusted().contains(uuid)
+                || plot.getMembers().contains(uuid);
     }
 
     public static String getPlotKey(Plot plot) {
